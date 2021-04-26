0b9c9b9a54cf642624e89cd59cd9fc41a262564b
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-26 17:08:23 +0530

    Created: Indian States Census Analyser

---
+++ src/main/java/com/censusanalyser/IndianStatesCensusAnalyser.java
@@ -0,0 +1,8 @@
+package com.censusanalyser;
+
+public class IndianStatesCensusAnalyser {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome Indian States Census Analyser");
+    }
+}
