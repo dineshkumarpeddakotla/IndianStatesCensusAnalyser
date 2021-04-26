3e289fce978173e523f9f2ee1f1fc4c9e7815be9
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-26 19:44:01 +0530

    Added: loadIndianStatesCensusData Method to Count the Indian States Census Data

--- src/main/java/com/censusanalyser/IndianStatesCensusAnalyser.java
+++ src/main/java/com/censusanalyser/IndianStatesCensusAnalyser.java
@@ -1,8 +1,42 @@
 package com.censusanalyser;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.StreamSupport;
+
 public class IndianStatesCensusAnalyser {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome Indian States Census Analyser");
+    /**
+     *
+     * @param csvFilePath  *.csv File Path
+     * @return Count of Indian Census Data
+     * @throws CensusAnalyserException Custom Exception is Thrown of CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE
+     */
+    public int loadIndianStatesCensusData(String csvFilePath) throws CensusAnalyserException {
+        try(Reader reader = Files.newBufferedReader(Paths .get(csvFilePath))) {
+            CsvToBeanBuilder<CensusAnalyser> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(CensusAnalyser.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<CensusAnalyser> csvToBean = csvToBeanBuilder.build();
+            Iterator<CensusAnalyser> censusCSVIterator = csvToBean.iterator();
+            Iterable<CensusAnalyser> csvIterable = () -> censusCSVIterator;
+            return (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (IllegalStateException e) {
+            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+        }
+    }
+
+    public static void main(String[] args) throws CensusAnalyserException {
+        System.out.println("Welcome to Indian States Census Analyser");
+        IndianStatesCensusAnalyser indianStatesCensusAnalyser = new IndianStatesCensusAnalyser();
+        int count = indianStatesCensusAnalyser.loadIndianStatesCensusData("C:\\Users\\dinnu\\FellowhShipProblems\\CenusAnalyser\\src\\main\\resources\\india-districts-census-2011.csv");
+        System.out.println("Indian States Census Data Count = " +count);
     }
 }
